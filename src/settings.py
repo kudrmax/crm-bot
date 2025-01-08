ae66bcbe122968ce9ce8c0532cf1025b92f90563
Maksim Kudryashov <mdmkudryashov@avito.ru>
2025-01-08 22:26:24 +0300

    Remove everything except bot

--- src/settings.py
+++ src/settings.py
@@ -8,32 +8,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 
-class DatabaseBase:
-    @property
-    def url(self) -> str:
-        return str(PostgresDsn.build(
-            scheme="postgresql+asyncpg",
-            username=self.user,
-            password=self.password,
-            host=self.host,
-            port=self.docker_port,
-            path=self.database,
-        ))
-
-    @property
-    def url_alembic(self) -> str:
-        res = str(PostgresDsn.build(
-            scheme="postgresql",
-            username=self.user,
-            password=self.password,
-            host='0.0.0.0',
-            port=self.port,
-            path=self.database,
-        ))
-        print(res)
-        return res
-
-
 class MyBaseSettings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file='.env',
@@ -57,43 +31,9 @@ class Telegram(MyBaseSettings):
         env_prefix = 'BOT_'
 
 
-class PostgresProd(MyBaseSettings, DatabaseBase):
-    host: str = 'db'
-    port: int
-    docker_port: int = 5432
-    user: str
-    password: str
-    database: str
-
-    class Config:
-        env_prefix = 'POSTGRES_'
-
-
-class PostgresTest(MyBaseSettings, DatabaseBase):
-    host: str = 'db_test'
-    port: int
-    docker_port: int = 5432
-    user: str
-    password: str
-    database: str
-
-    class Config:
-        env_prefix = 'POSTGRES_TEST_'
-
-
-class App(BaseSettings):
-    title: str = 'Contact CRM by kudrmax'
-    description: str = 'API, которое позволяет управлять контактами (создавать, удалять редактировать), вести "лог" взаимодействий с контактами, например зафиксировать что было на встрече, а также получать статистику по взаимодействиям, например как давно вы виделись с контактом.'
-    version: str = "0.0.1"
-    contact: Dict[str, str] = {"name": "Max Kudryashov", "email": "mdkudryashov@edu.hse.ru"}
-
-
 class Settings(BaseModel):
     server: Server = Server()
     telegram_bot: Telegram = Telegram()
-    db_prod: PostgresProd = PostgresProd()
-    db_test: PostgresTest = PostgresTest()
-    app: App = App()
 
 
 settings = Settings()
